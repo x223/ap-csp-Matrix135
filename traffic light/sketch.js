127e267330d7d8a8581a3aec2a1b273d8acad355
Terrell <terrell.skerritt@ms223.org>
2016-10-14 14:28:10 -0400

    Add files via upload

---
+++ traffic light/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background("purple");
+ fill("black");
+ rect(140,70,100,200);
+ fill("pink")
+ ellipse(190,100,50,50);
+ ellipse(190,,50,50)
+}
